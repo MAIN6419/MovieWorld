cada9f3b92b6e95b50dba015fe11820d4e194e28
jongtae.nam <dark9737@gmail.com>
2023-07-06 19:19:44 +0900

    Feat: React-router-dom 설치 및 router 설정 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Splash from "./pages/splash/Splash";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import FindAccount from "./pages/findAccount/FindAccount";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
+import Mypage from "./pages/mypage/Mypage";
 
 function App() {
- 
   return (
-    <GlobalStyles />
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/findAccount" element={<FindAccount />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
