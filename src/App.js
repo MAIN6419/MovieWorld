3fde0f2e7d95dc395c6b812c0ab6bc8155c52749
jongtae.nam <dark9737@gmail.com>
2023-07-06 20:23:02 +0900

    Feat: Layout NavBar, Banner, Footer 추가 및 router 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import GlobalStyles from "./GlobalStyles";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import Splash from "./pages/splash/Splash";
 import Login from "./pages/login/Login";
@@ -8,19 +8,33 @@ import FindAccount from "./pages/findAccount/FindAccount";
 import Home from "./pages/home/Home";
 import Search from "./pages/search/Search";
 import Mypage from "./pages/mypage/Mypage";
+import NavBar from "./compoents/commons/layouts/navBar/NavBar";
+import Banner from "./compoents/commons/layouts/Banner/Banner";
+import Footer from "./compoents/commons/layouts/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/findAccount" element={<FindAccount />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/mypage" element={<Mypage />} />
+        <Route
+          element={
+            <>
+              <NavBar />
+              <Banner />
+              <Outlet />
+              <Footer />
+            </>
+          }
+        >
+          <Route path="/" element={<Splash />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/findAccount" element={<FindAccount />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
