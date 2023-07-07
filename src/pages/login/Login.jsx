37dd0761a6404cabe02b1916be531939b60ff86b
jongtae.nam <dark9737@gmail.com>
2023-07-07 17:02:34 +0900

    Style: Login 페이지 마크업 및 스타일 완성

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  LoginBtn,
+  LoginForm,
+  LoginFormTitle,
+  Title,
+  Wrapper,
+  SignupLink,
+  FindAccountLink,
+  SignupText,
+} from "./login.style";
+import UserInput from "../../compoents/commons/userInput/UserInput";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <Title className="a11y-hidden">로그인 페이지</Title>
+      <Wrapper>
+        <LoginForm>
+          <LoginFormTitle>로그인</LoginFormTitle>
+          <UserInput
+            label={"이메일"}
+            id={"input-email"}
+            placeholder={"Email"}
+          />
+          <UserInput
+            label={"비밀번호"}
+            id={"input-password"}
+            placeholder={"Password"}
+          />
+          <FindAccountLink to={"/findAccount"}>아이디 | 비밀번호 찾기</FindAccountLink>
+          <LoginBtn>로그인</LoginBtn>
+
+          <SignupText>
+            아이디가 없으신가요? <SignupLink to={"/signup"}>회원가입</SignupLink>
+          </SignupText>
+        </LoginForm>
+      </Wrapper>
+    </>
+  );
 }
---
+++ src/pages/login/login.style.js
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Title = styled.h1``;
+export const Wrapper = styled.main`
+  background: url("assets/login-bg.png") no-repeat center / cover;
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100vw;
+  height: 100vh;
+`;
+export const LoginFormTitle = styled.h2`
+  color: #fff;
+  text-align: center;
+  font-size: 30px;
+  margin-bottom: 20px;
+  font-weight: 500;
+`;
+export const LoginForm = styled.form`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  max-width: 400px;
+  width: calc(100% - 60px);
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 30px;
+`;
+
+export const LoginBtn = styled.button`
+  width: 100%;
+  background-color: #e50914;
+  padding: 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  margin-top: 20px;
+`;
+
+export const SignupText = styled.p`
+  display: inline-block;
+  font-size: 12px;
+  color: #fff;
+`;
+
+export const SignupLink = styled(Link)`
+  color: #b3b3b3;
+  font-size: 12px;
+  margin-left: 3px;
+`;
+
+export const FindAccountLink = styled(Link)`
+  color: #b3b3b3;
+  font-size: 12px;
+  align-self: flex-end;
+`;
