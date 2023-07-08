644cc38003f585f27dbfec023a9880eb2820a98a
jongtae.nam <dark9737@gmail.com>
2023-07-08 17:21:53 +0900

    Feat: signup 페이지 마크업 및 스타일 완성, 유효성 검사 추가

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,160 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  SignupBtn,
+  SignupForm,
+  SignupTitle,
+  Title,
+  Wrapper,
+} from "./signup.style";
+import UserInput from "../../compoents/commons/userInput/UserInput";
+import { useValidationInput } from "../../hook/useValidationInput";
+import ErrorMsg from "../../compoents/commons/errorMsg/ErrorMsg";
 
 export default function Signup() {
+  const emailReg = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+  const nicknameReg = /^[a-zA-z0-9]{4,10}$/;
+  const passwordReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+  const phoneReg = /01[016789]-[^0][0-9]{2,3}-[0-9]{3,4}/;
+
+  // 닉네임 유효성 input
+  const [
+    nickNameValue,
+    setNickNameValue,
+    nickNameValid,
+    setNickNameValid,
+    onChangeNickName,
+  ] = useValidationInput(
+    "",
+    nicknameReg,
+    "4-10자 영문, 영문+숫자를 입력해주세요."
+  );
+
+  // 이메일 유효성 input
+  const [emailValue, setEmailValue, emailValid, setEmailValid, onChangeEmail] =
+    useValidationInput("", emailReg, "유효한 이메일을 입력해주세요.");
+  const [
+    passowrdValue,
+    setPassowrdValue,
+    passowrdValid,
+    setPassowrdValid,
+    onChangePassowrd,
+  ] = useValidationInput(
+    "",
+    passwordReg,
+    "8-16자 특수문자, 숫자, 영문을 포함해야합니다."
+  );
+
+  // 비밀번호 유효성 input
+  const [
+    passowrdChkValue,
+    setPasswordChkValue,
+    passwordChkValid,
+    setPasswordChkValid,
+    _,
+  ] = useValidationInput("", passwordReg, "비밀번호가 일치하지 않습니다.");
+
+  // 휴대폰 유효성 input
+  const [phoneValue, setPhoneValue, phoneValid, setPhoneValid, onChangePhone] =
+    useValidationInput("", phoneReg, "유효한 휴대폰 번호를 입력하세요.");
+
+  // 회원가입 버튼 활성화 상태 관리
+  const [disabled, setDisabled] = useState(true);
+
+  // 비밀번호 확인 onChange 별도 생성 => useValidInput에서 처리하지 못하기 때문
+  const onChangePasswordChk = (e) => {
+    setPasswordChkValue(e.target.value.trim());
+    if (passowrdValue !== e.target.value) {
+      setPasswordChkValid({
+        errorMsg: "비밀번호가 일치하지 않습니다.",
+        valid: false,
+      });
+    } else {
+      setPasswordChkValid({ errorMsg: "", valid: true });
+    }
+  };
+
+  // 전체 input이 유효하다면 버튼 활성화
+  useEffect(() => {
+    if (
+      nickNameValid &&
+      emailValid.valid &&
+      passowrdValid.valid &&
+      passwordChkValid.valid &&
+      phoneValid.valid
+    ) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [nickNameValid, emailValid, passowrdValid, passwordChkValid, phoneValid]);
+
   return (
-    <div>Signup</div>
-  )
+    <Wrapper>
+      <Title className="a11y-hidden">회원가입 페이지</Title>
+      <SignupForm>
+        <SignupTitle>회원가입</SignupTitle>
+        <UserInput
+          type="text"
+          label={"닉네임"}
+          id={"input-nickname"}
+          placeholder={"Nickname"}
+          value={nickNameValue}
+          onChange={onChangeNickName}
+          minLength={4}
+          maxLength={10}
+        />
+        {nickNameValid.errorMsg && (
+          <ErrorMsg message={nickNameValid.errorMsg} />
+        )}
+        <UserInput
+          type="text"
+          label={"이메일"}
+          id={"input-email"}
+          placeholder={"Email"}
+          value={emailValue}
+          onChange={onChangeEmail}
+        />
+        {emailValid.errorMsg && <ErrorMsg message={emailValid.errorMsg} />}
+        <UserInput
+          type="password"
+          label={"비밀번호"}
+          id={"input-password"}
+          placeholder={"Password"}
+          value={passowrdValue}
+          onChange={onChangePassowrd}
+          minLength={8}
+          maxLength={16}
+        />
+        {passowrdValid.errorMsg && (
+          <ErrorMsg message={passowrdValid.errorMsg} />
+        )}
+        <UserInput
+          type="password"
+          label={"비밀번호 확인"}
+          id={"input-passwordChk"}
+          placeholder={"Password check"}
+          value={passowrdChkValue}
+          onChange={onChangePasswordChk}
+          minLength={8}
+          maxLength={16}
+        />
+        {passwordChkValid.errorMsg && (
+          <ErrorMsg message={passwordChkValid.errorMsg} />
+        )}
+        <UserInput
+          type="text"
+          label={"휴대폰"}
+          id={"input-phone"}
+          placeholder={"Phone ( - 제외 )"}
+          value={phoneValue
+            .replace(/[^0-9]/g, "")
+            .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)}
+          onChange={onChangePhone}
+          maxLength={13}
+        />
+        {phoneValid.errorMsg && <ErrorMsg message={phoneValid.errorMsg} />}
+        <SignupBtn disabled={disabled}>회원가입</SignupBtn>
+      </SignupForm>
+    </Wrapper>
+  );
 }
---
+++ src/pages/signup/signup.style.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background: url("assets/signup-bg.jpg") no-repeat center / cover;
+`;
+
+export const Title = styled.h1``;
+
+export const SignupForm = styled.form`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 400px;
+  width: calc(100% - 60px);
+  background-color: rgba(0, 0, 0, 0.8);
+  padding: 30px 40px;
+  @media screen and (max-width: 431px) {
+    width: calc(100% - 40px);
+    padding: 30px 20px;
+  }
+`;
+
+export const SignupTitle = styled.h2`
+  color: #fff;
+  text-align: center;
+  font-size: 30px;
+  margin-bottom: 20px;
+  font-weight: 500;
+`;
+
+export const SignupBtn = styled.button`
+  width: 100%;
+  background-color: ${(props) =>
+    props.disabled ? "rgba(220,220,220, 0.5)" : "#e50914"};
+  cursor: ${(props) => (props.disabled ? "default" : "cursor")};
+  padding: 14px 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  margin-top: 20px;
+  transition: all 0.5s;
+`;
