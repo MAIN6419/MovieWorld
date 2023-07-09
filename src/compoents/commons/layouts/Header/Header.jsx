4b67ba955b5800f7bc49bdf769ef47333c5e1992
jongtae.nam <dark9737@gmail.com>
2023-07-09 14:48:51 +0900

    Feat: Header 공통 컴포넌트 완성

--- src/compoents/commons/layouts/Header/Header.jsx
+++ src/compoents/commons/layouts/Header/Header.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  HeaderBar,
+  HeaderLink,
+  HeaderLinks,
+  HeaderLogo,
+  HeaderLogoLink,
+  HeaderRight,
+  HeaderTitle,
+  UserMenu,
+  UserMenuBtn,
+  UserMenuBtnIcon,
+  UserMenuLi,
+  UserMenuItemBtn,
+  UserMenuItemLink,
+  UserNickname,
+  UserNicknameWrapper,
+  UserProfileImg,
+} from "./header.style";
+import { UserContext } from "../../../../context/userContext";
+import { logout } from "../../../../firebase/auth";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [isUserMenu, setIsUserMenu] = useState(false);
+
+  
+  const { user } = useContext(UserContext);
+
+
+  const onClickUserMenu = () => {
+    setIsUserMenu(!isUserMenu);
+  };
+  return (
+    <HeaderBar>
+      <HeaderTitle>
+        <HeaderLogoLink to="/home">
+          <HeaderLogo src="assets/logo.png" alt="MovieWorld" />
+        </HeaderLogoLink>
+      </HeaderTitle>
+      <HeaderRight>
+        {user && (
+          <UserProfileImg
+            src={user.photoURL || "assets/defultProfile.png"}
+            onError={(e)=>e.target.src="assets/defultProfile.png"}
+            alt="유저 프로필 이미지"
+          />
+        )}
+        <HeaderLinks>
+          {user ? (
+            <UserNicknameWrapper>
+              <UserNickname>
+                {user.displayName} 님
+                {isUserMenu && (
+                  <UserMenu>
+                    <UserMenuLi>
+                      <UserMenuItemLink to="/mypage">내 정보</UserMenuItemLink>
+                    </UserMenuLi>
+                    <UserMenuLi>
+                      <UserMenuItemBtn onClick={logout}>로그아웃</UserMenuItemBtn>
+                    </UserMenuLi>
+                  </UserMenu>
+                )}
+              </UserNickname>
+              <UserMenuBtn  onClick={onClickUserMenu}>
+                <UserMenuBtnIcon src={"assets/icon-downArrow.png"} active={isUserMenu} alt="유저 메뉴 버튼"/>
+              </UserMenuBtn>
+            </UserNicknameWrapper>
+          ) : (
+            <>
+              <HeaderLink to="/login">로그인</HeaderLink>
+              <HeaderLink to="/signup">회원가입</HeaderLink>
+            </>
+          )}
+        </HeaderLinks>
+      </HeaderRight>
+    </HeaderBar>
+  );
 }
---
+++ src/compoents/commons/layouts/Header/header.style.js
@@ -0,0 +1,138 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const HeaderBar = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: #000;
+  padding: 20px;
+  color: #fff;
+`;
+
+export const HeaderTitle = styled.h1``;
+
+export const HeaderLogoLink = styled(Link)``;
+
+export const HeaderLogo = styled.img`
+  width: 150px;
+  @media screen and (max-width: 440px) {
+    width: 120px;
+  }
+`;
+
+export const HeaderRight = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  margin-right: 10px;
+  font-size: 14px;
+`;
+
+export const HeaderLinks = styled.div``;
+
+export const HeaderLink = styled(Link)`
+  color: #fff;
+  margin-right: 8px;
+  :first-child::after {
+    content: "";
+    width: 1px;
+    height: 12px;
+    background-color: #fff;
+    display: inline-block;
+    margin-left: 8px;
+  }
+`;
+
+export const UserNicknameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  position: relative;
+`;
+
+export const UserNickname = styled.strong`
+  color: #fff;
+  font-weight: 500;
+`;
+
+export const UserProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border: 1px solid #bdbdbd;
+  @media screen and (max-width: 440px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+export const UserMenuBtn = styled.button`
+  width: 20px;
+  height: 20px;
+  background: none;
+`;
+
+export const UserMenuBtnIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  transform: ${(props) => (props.active ? "rotate(-180deg)" : "rotate(0deg)")};
+  transition: all 0.5s;
+`;
+
+export const UserMenu = styled.ul`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  width: 90px;
+  border: 1px solid #bdbdbd;
+  border-radius: 5px;
+  background: #555;
+  overflow: hidden;
+  animation: fadeIn 0.5s;
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+export const UserMenuLi = styled.li`
+  transition: all 0.3s;
+  :not(:last-child) {
+    border-bottom: 1px solid #fff;
+  }
+
+  :hover {
+    background-color: #739bfa;
+  }
+`;
+
+export const UserMenuItemLink = styled(Link)`
+  padding: 15px 8px;
+  font-size: 16px;
+  display: block;
+  border: none;
+  width: 100%;
+  font-weight: 400;
+  cursor: pointer;
+`;
+
+export const UserMenuItemBtn = styled.button`
+  padding: 11.5px 8px;
+  display: block;
+  border: none;
+  width: 100%;
+  background: none;
+  color: #fff;
+  text-align: left;
+  font-size: 16px;
+  font-weight: 400;
+  cursor: pointer;
+`;
