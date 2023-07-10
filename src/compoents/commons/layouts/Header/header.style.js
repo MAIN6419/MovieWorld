0093de3489cfe270864e1ffe7bd8b21a20bd4add
jongtae.nam <dark9737@gmail.com>
2023-07-10 11:44:04 +0900

    Style: header 공통 컴포넌트 스타일 postition 속성 수정

--- src/compoents/commons/layouts/Header/header.style.js
+++ src/compoents/commons/layouts/Header/header.style.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const HeaderBar = styled.header`
-  position: fixed;
+  position: sticky;
   top: 0;
   left: 0;
   width: 100%;
