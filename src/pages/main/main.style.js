7062dfef0a095d4d81d1256889d7689a806f2606
jongtae.nam <dark9737@gmail.com>
2023-07-11 21:50:09 +0900

    Style: Main 페이지 스타일 수정

--- src/pages/main/main.style.js
+++ src/pages/main/main.style.js
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   background-color: #111;
   padding: 20px 0;
+  height: auto;
+  min-height: 100%;
+  padding-bottom: 260px;
 `
