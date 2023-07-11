9864a8a1bd46e8017616e36a6fa18d72c6263d2b
jongtae.nam <dark9737@gmail.com>
2023-07-11 16:50:44 +0900

    Feat: 추가적인 데이터를 받기위해 videoData를 받아오는 로직 추가 및 기존 movieData를 videoData로 변경

--- src/compoents/commons/Modal/MovieInfo.jsx
+++ src/compoents/commons/Modal/MovieInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CloseBtn,
   ModalCard,
@@ -19,15 +19,34 @@ import {
   MovieContetns,
   IframeWrapper,
   Iframe,
+ 
 } from "./movieInfo.style";
+import { fetchVideo } from "../../../api/movie";
 
 export default function MovieInfo({ movieData, setIsOpenMovieInfo }) {
   const [isPlay, setIsPlay] = useState(false);
-
+  const [videoData, setVideoData] = useState({});
   const onClickClose = () => {
     setIsOpenMovieInfo(false);
     document.body.style.overflow = "auto";
   };
+
+  const fetchData = async () => {
+    const data = await fetchVideo(movieData.id);
+    setVideoData(data);
+  };
+
+  const onClickPlay = () => {
+    if (!videoData.videos.results.length) {
+      alert("영상이 존재하지 않습니다!");
+      return;
+    }
+    setIsPlay(true);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <ModalWrapper>
@@ -37,21 +56,21 @@ export default function MovieInfo({ movieData, setIsOpenMovieInfo }) {
         <MovieImgWrapper>
           {isPlay ? (
             <>
-                <IframeWrapper>
-                  <Iframe
-                    src={`https://www.youtube.com/embed/${movieData.videos.results[0].key}?autoplay=1&mute=1&loop=1&playlist=${movieData.videos.results[0].key}`}
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;"
-                    allowfullscreen
-                  />
-                </IframeWrapper>
+              <IframeWrapper>
+                <Iframe
+                  src={`https://www.youtube.com/embed/${videoData.videos.results[0].key}?autoplay=1&mute=1&loop=1&playlist=${videoData.videos.results[0].key}`}
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;"
+                  allowfullscreen
+                />
+              </IframeWrapper>
             </>
           ) : (
             <MovieImg
               style={
-                movieData.backdrop_path && {
-                  background: `url(https://image.tmdb.org/t/p/original/${movieData.backdrop_path}) no-repeat top center / cover`,
+                videoData.backdrop_path && {
+                  background: `url(https://image.tmdb.org/t/p/original/${videoData.backdrop_path}) no-repeat top center / cover`,
                 }
               }
             ></MovieImg>
@@ -59,29 +78,32 @@ export default function MovieInfo({ movieData, setIsOpenMovieInfo }) {
         </MovieImgWrapper>
         <MovieContetns>
           <MovieTitle>
-            {movieData.title || movieData.name || movieData.original_name}
+            {videoData.title || videoData.name || videoData.original_name}
           </MovieTitle>
-          <MovieRelease dateTime={movieData.release_date}>
+          <MovieRelease dateTime={videoData.release_date}>
             개봉 :{" "}
-            {movieData.release_date
-              ? movieData.release_date
-              : movieData.first_air_date}
+            {videoData.release_date
+              ? videoData.release_date
+              : videoData.first_air_date}
           </MovieRelease>
           <MovieGenre>
             장르 :
-            {movieData.genres.map(({ name }) => {
-              return <MovieGenreLi key={name}>{name}</MovieGenreLi>;
-            })}
+            {videoData.genres &&
+              videoData.genres.map(({ name }) => {
+                return <MovieGenreLi key={name}>{name}</MovieGenreLi>;
+              })}
           </MovieGenre>
-          <MovieRunTime>상영시간 : {movieData.runtime}분</MovieRunTime>
+          {videoData.runtime && (
+            <MovieRunTime>상영시간 : {videoData.runtime}분</MovieRunTime>
+          )}
           <MovieRating>
             평점 : <MovieRatingIcon />{" "}
-            {parseFloat(movieData.vote_average).toFixed(2)}
+            {parseFloat(videoData.vote_average).toFixed(2)}
           </MovieRating>
-          <MoviePlayBtn onClick={()=>setIsPlay(true)}>재생</MoviePlayBtn>
+          <MoviePlayBtn onClick={onClickPlay}>재생</MoviePlayBtn>
           <MovieDesc>
-            {movieData.overview
-              ? movieData.overview
+            {videoData.overview
+              ? videoData.overview
               : "영화에 대한 설명이 없습니다."}
           </MovieDesc>
         </MovieContetns>
