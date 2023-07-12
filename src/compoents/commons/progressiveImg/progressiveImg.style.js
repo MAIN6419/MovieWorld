afce351062de0660e2020e7d4e411194bc3fe046
jongtae.nam <dark9737@gmail.com>
2023-07-12 20:00:54 +0900

    Style: ProgressiveImg 페이지 스타일 수정

--- src/compoents/commons/progressiveImg/progressiveImg.style.js
+++ src/compoents/commons/progressiveImg/progressiveImg.style.js
@@ -2,6 +2,7 @@ import { isMobile } from "react-device-detect";
 import styled from "styled-components";
 
 export const Img = styled.img`
+  transition: transform 0.4s;
   :hover {
     transform: ${isMobile ? "" : "scale(1.05)"};
   }
