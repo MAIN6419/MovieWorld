33c866571e2bdd01a2be85877c01a4d29d150b32
jongtae.nam <dark9737@gmail.com>
2023-07-14 17:37:56 +0900

    Feat: 유저 프로필 수정 API 추가 및 nickname를 dispalyName, profileImgUrl를 photoURL, profileFileName으로 수정

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -24,23 +24,32 @@ import {
   signOut,
 } from "firebase/auth";
 import { db, storage } from "./setting";
-export const auth = getAuth();
+import {
+  uploadBytes,
+  getDownloadURL,
+  ref as storageRef,
+  deleteObject,
+} from "firebase/storage";
 
+import { v4 as uuidv4 } from "uuid";
+
+const auth = getAuth();
+const userData = JSON.parse(localStorage.getItem("user"));
 // 로그인 변화를 감지
 // 로그인이 된 경우 로컬스토리지에 필요한 유저정보 저장
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    localStorage.setItem(
-      "user",
-      JSON.stringify({
-        uid: user.uid,
-        displayName: user.displayName,
-        email: user.email,
-        photoURL: user.photoURL,
-      })
-    );
-  }
-});
+// onAuthStateChanged(auth, async (user) => {
+//   if (user) {
+//     localStorage.setItem(
+//       "user",
+//       JSON.stringify({
+//         uid: user.uid,
+//         displayName: user.displayName,
+//         email: user.email,
+//         photoURL: user.photoURL,
+//       })
+//     );
+//   }
+// });
 
 // 로그인 API
 export const login = async (email, password) => {
@@ -99,24 +108,31 @@ export const duplication = async (duplicationValue, duplicationTarget) => {
 };
 
 // 회원가입 API
-export const signup = async (nickname, email, password, phone) => {
+export const signup = async (displayName, email, password, phone) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     await updateProfile(res.user, {
-      displayName: nickname,
+      displayName,
     });
 
     const user = collection(db, "user");
-    await setDoc(doc(user, `${res.user.displayName ?? ""}`), {
+    await setDoc(doc(user, `${res.user.uid}`), {
       email: res.user.email,
-      nickname: res.user.displayName,
+      displayName: res.user.displayName,
       phone,
       likeList: [],
-      profileImgFileName: "",
-      profileImgUrl: "",
+      photoFileName: "",
+      photoURL: "",
     });
-    alert("회원가입이 완료되었습니다.");
-    window.location.replace("/main");
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        uid: user.uid,
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+      })
+    );
   } catch (error) {
     if (error.message.includes("email-already-in-use")) {
       alert("이미 사용중인 이메일 입니다!");
@@ -128,12 +144,12 @@ export const signup = async (nickname, email, password, phone) => {
 };
 
 // 이메일 찾기 API
-export const findEmail = async (nickname, phone) => {
+export const findEmail = async (displayName, phone) => {
   try {
     const userRef = collection(db, "user");
     const q = query(
       userRef,
-      where("nickname", "==", nickname),
+      where("displayName", "==", displayName),
       where("phone", "==", phone)
     );
     const res = await getDocs(q);
@@ -177,15 +193,79 @@ export const changePassword = async (email, phone) => {
   }
 };
 
+// 유저 프로필 수정 API
+
+export const updateUserProfile = async (file, displayName) => {
+  try {
+    const fileName = file && `${uuidv4()}_${file.name}`;
+    const res =
+      file &&
+      (await uploadBytes(
+        storageRef(storage, `images/profile/${fileName}`),
+        file
+      ));
+    const user = await getUser();
+    const uploadfileUrl = file && (await getDownloadURL(res.ref));
+    const updateUser = doc(db, `user/${auth.currentUser.uid}`);
+    // 수정 사항의 경우 닉네임과 이미지 모두 변경, 닉네임만 변경, 이미지만 변경할 경우를 나눔
+    // 필요만 API만 사용하여 불필요한 API 사용을 방지
+
+    // 1. 닉네임과 이미지 파일이 모두 변경된 경우
+    if (displayName && file) {
+      if (user.photoFileName)
+        await deleteObject(
+          storageRef(storage, `images/profile/${String(user.photoFileName)}`)
+        );
+      await updateProfile(auth.currentUser, {
+        displayName,
+        photoURL: uploadfileUrl,
+      });
+      await updateDoc(updateUser, {
+        displayName,
+        photoFileName: fileName,
+        photoURL: uploadfileUrl,
+      });
+      userData.displayName = displayName;
+      userData.photoURL = uploadfileUrl;
+    } 
+
+    // 2. 닉네임만 변경된 경우
+    else if (displayName && !file) { 
+      await updateProfile(auth.currentUser, { displayName });
+      await updateDoc(updateUser, {
+        displayName,
+      });
+      userData.displayName = displayName;
+    } 
+
+    // 3. 이미지만 변경된 경우
+    else if (!displayName && file) { 
+      if (user.photoFileName)
+        await deleteObject(
+          storageRef(storage, `images/profile/${String(user.photoFileName)}`)
+        );
+      await updateProfile(auth.currentUser, { photoURL: uploadfileUrl });
+      await updateDoc(updateUser, {
+        photoFileName: fileName,
+        photoURL: uploadfileUrl,
+      });
+      userData.photoURL = uploadfileUrl;
+    }
+
+    localStorage.setItem("user", JSON.stringify(userData));
+  } catch (error) {
+    alert("알 수 없는 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.");
+    throw error;
+  }
+};
+
 // 유저 데이터 API
 export const getUser = async () => {
   try {
-    if (auth.currentUser) {
-      const userRef = doc(db, `user/${auth.currentUser.displayName}`);
-      const res = await getDoc(userRef);
-      const data = res.data();
-      return data;
-    }
+    const userRef = doc(db, `user/${userData.uid}`);
+    const res = await getDoc(userRef);
+    const data = res.data();
+    return data;
   } catch (error) {
     alert("알 수 없는 에러가 발생하였습니다. 잠시후 다시 시도해 주세요.");
     throw error;
@@ -200,7 +280,7 @@ export const addLike = async (movieData) => {
       await setDoc(doc(userLikeRef, String(movieData.id)), {
         ...movieData,
       });
-      const userRef = doc(db, `user/${auth.currentUser.displayName}`);
+      const userRef = doc(db, `user/${auth.currentUser.uid}`);
       await updateDoc(userRef, {
         likeList: arrayUnion(movieData.id),
       });
@@ -220,7 +300,7 @@ export const removeLike = async (movieData) => {
     if (auth.currentUser) {
       const userLikeRef = collection(db, `${auth.currentUser.uid}_LikeList`);
       await deleteDoc(doc(userLikeRef, String(movieData.id)));
-      const userRef = doc(db, `user/${auth.currentUser.displayName}`);
+      const userRef = doc(db, `user/${auth.currentUser.uid}`);
       await updateDoc(userRef, {
         likeList: arrayRemove(movieData.id),
       });
@@ -237,9 +317,8 @@ export const removeLike = async (movieData) => {
 // 좋아요 목록 API
 export const fetchFirstLikeList = async (limitPage) => {
   try {
-    const uid = JSON.parse(localStorage.getItem("user")).uid;
-    const userRef = collection(db, `${uid}_LikeList`);
-    const q = query(userRef, limit(limitPage))
+    const userRef = collection(db, `${userData.uid}_LikeList`);
+    const q = query(userRef, limit(limitPage));
     const res = await getDocs(q);
     return res;
   } catch (error) {
@@ -251,8 +330,7 @@ export const fetchFirstLikeList = async (limitPage) => {
 // 좋아요 목록 페이징 API
 export const fetchLikeListPage = async (page, limitPage) => {
   try {
-    const uid = JSON.parse(localStorage.getItem("user")).uid;
-    const userRef = collection(db, `${uid}_LikeList`);
+    const userRef = collection(db, `${userData.uid}_LikeList`);
     const q = query(userRef, startAfter(page), limit(limitPage));
     const res = await getDocs(q);
     return res;
