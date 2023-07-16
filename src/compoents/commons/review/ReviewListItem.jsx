fa4838367eea35a8ba6f1560e5f273c40c08a652
jongtae.nam <dark9737@gmail.com>
2023-07-17 00:05:47 +0900

    Feat: Review 삭제 기능 추가

--- src/compoents/commons/review/ReviewListItem.jsx
+++ src/compoents/commons/review/ReviewListItem.jsx
@@ -13,9 +13,22 @@ import {
   ReviewerWrapper,
 } from "./reviewListItem.style";
 import { setDateFormate } from "../../../libray/setDateFormate";
+import { removeReview } from "../../../firebase/auth";
 
-export default function ReviewListItem({reviewData}) {
-  console.log(reviewData)
+export default function ReviewListItem({
+  reviewData,
+  user,
+  movieId,
+  setReviewData,
+}) {
+  const onClickRemove = () => {
+    const answer = window.confirm("정말 삭제하시겠습니까?");
+    if (answer) {
+      removeReview(movieId, reviewData.id);
+      setReviewData((prev) => prev.filter((data) => data.id !== reviewData.id));
+    }
+  };
+
   return (
     <ReviewItem>
       <ReviewerWrapper>
@@ -23,13 +36,24 @@ export default function ReviewListItem({reviewData}) {
         <Reviewer>{reviewData.reviewer}</Reviewer>
         <ReviewItemRateWrapper>
           <ReviewItemRate defaultValue={reviewData.rating} disabled />
-          <ReviewItemRateCount>{reviewData.rating}</ReviewItemRateCount>
+          <ReviewItemRateCount>{reviewData.rating * 2}</ReviewItemRateCount>
         </ReviewItemRateWrapper>
       </ReviewerWrapper>
       <ReviewContents>{reviewData.contents}</ReviewContents>
       <ReviewItemBottom>
-        <ReviewCreatedAt>{setDateFormate(reviewData.createdAt.seconds * 1000)}</ReviewCreatedAt>
-        <ReviewItemBtn>신고</ReviewItemBtn>
+        <ReviewCreatedAt>
+          {setDateFormate(reviewData.createdAt.seconds * 1000)}
+        </ReviewCreatedAt>
+        {user.displayName === reviewData.reviewer ? (
+          <>
+            <ReviewItemBtn type="button">수정</ReviewItemBtn>
+            <ReviewItemBtn type="button" onClick={onClickRemove}>
+              삭제
+            </ReviewItemBtn>
+          </>
+        ) : (
+          <ReviewItemBtn>신고</ReviewItemBtn>
+        )}
       </ReviewItemBottom>
     </ReviewItem>
   );
