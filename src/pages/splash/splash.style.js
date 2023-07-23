45fd7d13bb0d861f835d60fac2a7b6966fc31fbf
jongtae.nam <dark9737@gmail.com>
2023-07-23 17:20:12 +0900

    Style: Splash 페이지 스타일 수정

--- src/pages/splash/splash.style.js
+++ src/pages/splash/splash.style.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 export const Title = styled.h1``
 export const Wrapper = styled.div`
+  position: absolute;
+  top:0;
+  left:0;
   width: 100vw;
   height: 100vh;
   background-color: #333;
