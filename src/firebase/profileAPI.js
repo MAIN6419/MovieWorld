48c8deac0112e831841aef7a1576a5447dadeea3
jongtae.nam <dark9737@gmail.com>
2023-07-30 17:35:20 +0900

    Feat: 프로필 이미지 수정 시 browser-image-compression 라이브러리를 이용하여 프로필 이미지 크기 압축 기능 추가

--- src/firebase/profileAPI.js
+++ src/firebase/profileAPI.js
@@ -8,6 +8,7 @@ import {
   deleteObject,
 } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
+import imageCompression from "browser-image-compression";
 import { getUser } from "./loginAPI";
 
 const auth = getAuth();
@@ -15,11 +16,19 @@ const auth = getAuth();
 export const updateUserProfile = async (file, displayName) => {
   try {
     const fileName = file && `${uuidv4()}_${file.name}`;
+    const options = {
+      maxSizeMB: 10, // 이미지 최대 용량
+      maxWidthOrHeight: 256, // 이미지 최대 너비 및 높이
+      useWebWorker: true, // webworker 적용 유무
+      // webworker : 웹 워커 API가 멀티 스레딩을 지원하게 되어 워커를 이용하면 워커에서 작성된 스크립트는 
+      // 메인 스레드에서 분기되어 독립된 스레드로 실행되기 때문에 메모리 자원을 효율적으로 사용할 수 있다.
+    };
+    const compressedFile = file && (await imageCompression(file, options));
     const res =
       file &&
       (await uploadBytes(
         storageRef(storage, `images/profile/${fileName}`),
-        file
+        compressedFile
       ));
     const user = await getUser();
     const uploadfileUrl = file && (await getDownloadURL(res.ref));
