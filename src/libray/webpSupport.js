06ce22c95b7cbeefeef68b35c4bc00fae33b4029
jongtae.nam <dark9737@gmail.com>
2023-08-07 22:33:21 +0900

    Fix: webpSupport 주석 추가 및 resolveWebp 함수 조건문 수정

--- src/libray/webpSupport.js
+++ src/libray/webpSupport.js
@@ -3,7 +3,7 @@ export function detectWebpSupport() {
   // 1px x 1px WebP 이미지
   const webpdata = "data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=";
   const callback = (event) => {
-    // if the event is from 'onload', check the see if the image's width is 1 pixel (which indicates support). otherwise, it fails
+    // event.type이 "load"인 경우와 이미지의 너비(image.width)가 1 픽셀인 경우를 검사하여 브라우저가 WebP 이미지를 지원하는지 여부를 판별
     const result = event?.type === "load" && image.width === 1;
     if (result) {
       document.body.classList.add("webp");
@@ -16,11 +16,12 @@ export function detectWebpSupport() {
   image.onload = callback;
   image.src = webpdata;
 }
-
-export const resolveWebp = (webpSupported,img, fallbackExt) => {
+// webpSupported: webp 지원 유무, img: webp 이미지 경로, fallbackExt: webp 이미지 대체 이미지 형식
+export const resolveWebp = (webpSupported, img, fallbackExt) => {
+  // 이미지 포맷
   const ext = img.split(".").pop();
-  if (!webpSupported && ext === "") {
-
+  // webpSupported false, ext가 webp인 경우
+  if (!webpSupported && ext === "webp") {
     return img.replace("/webp", "").replace(".webp", `.${fallbackExt}`);
   }
   return img;
