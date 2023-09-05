c3f365f3e63c7e9112507d4a867d070332a3050b
MAIN6419 <dark9737@gmail.com>
2023-09-06 01:00:27 +0900

    Refactor: Blank 컴포넌트 typescript 적용

--- src/compoents/commons/blank/Blank.tsx
+++ src/compoents/commons/blank/Blank.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { BlankImg, BlankText, Title, Wrapper } from "./blank.style";
 import { resolveWebp } from "../../../libray/webpSupport";
+interface IProps {
+  size: string;
+  text: string;
+}
 
-export default function Blank({ size, text }) {
+export default function Blank({ size, text }: IProps) {
   return (
     <Wrapper>
       <Title className="a11y-hidden">blank data</Title>
