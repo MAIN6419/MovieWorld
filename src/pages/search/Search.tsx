400bfa012b86c4f77837f736e49a024c44547304
MAIN6419 <dark9737@gmail.com>
2023-09-06 01:07:31 +0900

    Refactor: Search 컴포넌트 typescript 적용

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -12,7 +12,7 @@ import {
   SearchBlankImg,
   SearchBlankText,
   SearchBlankKeyword,
-  SearchMovieBtn,
+  SearchMovieBtn
 } from "./search.style";
 import { debounce } from "lodash";
 import MovieInfo from "../../compoents/commons/Modal/MovieInfo.container";
@@ -22,25 +22,30 @@ import TopButton from "../../compoents/commons/topButton/TopButton";
 import { useMovieInfo } from "../../hook/useMovieInfo";
 import { resolveWebp } from "../../libray/webpSupport";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSearchMovies, fetchTrendingPageMovies, searchSlice } from "../../slice/searchSlice";
+import {
+  fetchSearchMovies,
+  fetchTrendingPageMovies,
+  searchSlice
+} from "../../slice/searchSlice";
+import { AppDispatch, RootState } from '../../store/store';
 
 export default function Search() {
-  const searchInputRef = useRef(null);
-  const dispatch = useDispatch();
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch<AppDispatch>();
   // 현재 페이지를 관리
-  const page = useSelector((state) => state.search.page);
+  const page = useSelector((state: RootState) => state.search.page);
   // 다음 페이지가 있는지 확인
-  const hasMore = useSelector((state) => state.search.hasMore);
+  const hasMore = useSelector((state: RootState) => state.search.hasMore);
   const [keyword, setkeyword] = useState("");
-  const movieData = useSelector((state) => state.search.data);
+  const movieData = useSelector((state: RootState) => state.search.data);
   const [isOpenMovieInfo, setIsOpenMovieInfo, seletedMovie, onClickMovieInfo] =
     useMovieInfo(false);
   // 무한 스크롤 이용하기 위한 hook
   // ref는 관찰대상, inView 현재 관찰대상이 화면에 나타나지를 반환
-  const [ref, inVeiw] = useInView(false);
+  const [ref, inVeiw] = useInView();
   // 검색어가 있는 없는지를 파악
-  const isBlank = useSelector((state) => state.isBlank);
-  const onChangekeyword = (e) => {
+  const isBlank = useSelector((state: RootState) => state.search.isBlank);
+  const onChangekeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length === 1 && e.target.value === " ") {
       return;
     }
@@ -56,7 +61,7 @@ export default function Search() {
         return;
       }
       dispatch(searchSlice.actions.resetSearch());
-      searchInputRef.current.blur();
+      searchInputRef.current&&searchInputRef.current.blur();
     }, 500),
     []
   );
@@ -83,9 +88,9 @@ export default function Search() {
     <>
       <SearchWrapper>
         <SearchForm>
-          <SearchLabel className="a11y-hidden">검색</SearchLabel>
+          <SearchLabel className='a11y-hidden'>검색</SearchLabel>
           <SearchInput
-            placeholder="검색"
+            placeholder='검색'
             value={keyword}
             onChange={onChangekeyword}
             ref={searchInputRef}
@@ -97,7 +102,7 @@ export default function Search() {
               src={resolveWebp("/assets/webp/icon-blank.webp", "svg")}
             />
             <SearchBlankText>
-              <SearchBlankKeyword>"{keyword}"</SearchBlankKeyword>
+              <SearchBlankKeyword>&quot;{keyword}&quot;</SearchBlankKeyword>
               {" 에 대한\n검색 결과가 존재하지 않습니다."}
             </SearchBlankText>
           </SearchBlank>
@@ -110,7 +115,7 @@ export default function Search() {
                   data.poster_path && (
                     <SearchMovieItem key={data.id}>
                       <SearchMovieBtn
-                        type="button"
+                        type='button'
                         onClick={() => onClickMovieInfo(data)}
                       >
                         <SearchMovieImgWrapper>
@@ -123,10 +128,10 @@ export default function Search() {
                               height: "100%",
                               top: "0",
                               left: "0",
-                              borderRadius: "10px",
+                              borderRadius: "10px"
                             }}
-                            alt="영화 포스터"
-                            onError={(e) =>
+                            alt='영화 포스터'
+                            onError={(e: any) =>
                               (e.target.src = resolveWebp(
                                 "/assets/webp/placeholderImg.webp",
                                 "svg"
