38d935765761f1989da9beb54870f0a225120311
MAIN6419 <dark9737@gmail.com>
2023-09-06 01:04:33 +0900

    Refactor: MovieList 컴포넌트 typescript 적용

--- src/compoents/units/movieList/MovieList.tsx
+++ src/compoents/units/movieList/MovieList.tsx
@@ -8,7 +8,7 @@ import {
   MovieItemBtn,
   SwiperContainer,
   Title,
-  Wrapper,
+  Wrapper
 } from "./movieList.style";
 
 import MovieInfo from "../../commons/Modal/MovieInfo.container";
@@ -18,11 +18,32 @@ import ProgressiveImg from "../../commons/progressiveImg/ProgressiveImg";
 import { useMovieInfo } from "../../../hook/useMovieInfo";
 import { useMediaQuery } from "react-responsive";
 import { useDispatch, useSelector } from "react-redux";
+import { IMovieData } from "../../../api/movieAPIType";
 
-export default function MovieList({ title, type, fetchMoive }) {
+interface IProps {
+  title: string;
+  type: string;
+  fetchMoive: any;
+}
+
+interface IMovieDataState {
+  [key: string]: {
+    data: IMovieData[];
+    error: string;
+  };
+}
+
+// RootState 타입 재정의
+interface RootState {
+  movieData: IMovieDataState;
+}
+
+export default function MovieList({ title, type, fetchMoive }: IProps) {
   const isSmall = useMediaQuery({ query: "(max-width:500px)" });
   // type 값을 이용하여 해당되는 영화데이터를 가져옴
-  const movieData = useSelector((state) => state.movieData[type].data);
+  const movieData = useSelector(
+    (state: RootState) => state.movieData[type]?.data || []
+  );
   const dispatch = useDispatch();
   const [isOpenMovieInfo, setIsOpenMovieInfo, seletedMovie, onClickMovieInfo] =
     useMovieInfo(false);
@@ -44,28 +65,28 @@ export default function MovieList({ title, type, fetchMoive }) {
           breakpoints={{
             1900: {
               slidesPerView: 7,
-              slidesPerGroup: 7,
+              slidesPerGroup: 7
             },
             1378: {
               slidesPerView: 6,
-              slidesPerGroup: 6,
+              slidesPerGroup: 6
             },
             998: {
               slidesPerView: 5,
-              slidesPerGroup: 5,
+              slidesPerGroup: 5
             },
             500: {
               slidesPerView: 4,
-              slidesPerGroup: 4,
+              slidesPerGroup: 4
             },
             420: {
               slidesPerView: 3,
-              slidesPerGroup: 3,
+              slidesPerGroup: 3
             },
             0: {
               slidesPerView: 3,
-              slidesPerGroup: 3,
-            },
+              slidesPerGroup: 3
+            }
           }}
         >
           <MovieItemWrapper>
@@ -73,17 +94,19 @@ export default function MovieList({ title, type, fetchMoive }) {
               return (
                 <SwiperSlide
                   key={data.id}
-                  onKeyDown={(e) => {
-                    if (e.keyCode === 13) onClickMovieInfo(data);
+                  onKeyDown={(e: React.KeyboardEvent) => {
+                    if (e.keyCode === 13) {
+                      onClickMovieInfo(data);
+                    }
                   }}
                 >
                   <MovieItemBtn
                     onClick={() => {
                       onClickMovieInfo(data);
+                      const bannerIframe = document.querySelector(".bannerIframe") as HTMLIFrameElement;
                       // movieInfo 모달창을 열때 banner 동영상이 실행중 이라면 중지 시킴
-                      if (document.querySelector(".bannerIframe") !== null) {
-                        document
-                          .querySelector(".bannerIframe")
+                      if (bannerIframe&&bannerIframe.contentWindow !== null) {
+                        bannerIframe
                           .contentWindow.postMessage(
                             '{"event":"command","func":"pauseVideo","args":""}',
                             "*"
@@ -91,21 +114,23 @@ export default function MovieList({ title, type, fetchMoive }) {
                       }
                     }}
                   >
-                    {data.poster_path&&<ProgressiveImg
-                      placeholderSrc={`https://image.tmdb.org/t/p/w45/${data.poster_path}`}
-                      src={`https://image.tmdb.org/t/p/${
-                        isSmall ? "w185" : "w342"
-                      }${data.poster_path}`}
-                      styles={{
-                        objectFit: "contain",
-                        width: "100%",
-                        height: "100%",
-                        transition: "transform 0.45s",
+                    {data.poster_path && (
+                      <ProgressiveImg
+                        placeholderSrc={`https://image.tmdb.org/t/p/w45/${data.poster_path}`}
+                        src={`https://image.tmdb.org/t/p/${
+                          isSmall ? "w185" : "w342"
+                        }${data.poster_path}`}
+                        styles={{
+                          objectFit: "contain",
+                          width: "100%",
+                          height: "100%",
+                          transition: "transform 0.45s",
 
-                        borderRadius: "5px",
-                      }}
-                      alt="영화 포스터"
-                    />}
+                          borderRadius: "5px"
+                        }}
+                        alt={"영화 포스터"}
+                      />
+                    )}
                   </MovieItemBtn>
                 </SwiperSlide>
               );
